leaflabs/libmaple

commit 43d6921658cd29b8022af4424d340a90fbcb9a7f

Refactor linker scripts. Rename irq and exception handlers.
Add common linker scripts for ram and rom. Add medium and high density
libraries for libcs3.

libmaple/dma.c
@@ -77,31 +77,31 @@ static inline void dispatch_handler(uint8 channel_idx) {
     }
 }
 
-void DMAChannel1_IRQHandler(void) {
+void __irq_dma1_channel1(void) {
     dispatch_handler(0);
 }
 
-void DMAChannel2_IRQHandler(void) {
+void __irq_dma1_channel2(void) {
     dispatch_handler(1);
 }
 
-void DMAChannel3_IRQHandler(void) {
+void __irq_dma2_channel3(void) {
     dispatch_handler(2);
 }
 
-void DMAChannel4_IRQHandler(void) {
+void __irq_dma2_channel4(void) {
     dispatch_handler(3);
 }
 
-void DMAChannel5_IRQHandler(void) {
+void __irq_dma2_channel5(void) {
     dispatch_handler(4);
 }
 
-void DMAChannel6_IRQHandler(void) {
+void __irq_dma2_channel6(void) {
     dispatch_handler(5);
 }
 
-void DMAChannel7_IRQHandler(void) {
+void __irq_dma2_channel7(void) {
     dispatch_handler(6);
 }
 

support/ld/names.inc
@@ -1,61 +1,78 @@
-/* ISR names for STM32 (by Lanchon) */
+EXTERN(__cs3_stack)
+EXTERN(__cs3_reset)
+EXTERN(__exc_nmi)
+EXTERN(__exc_hardfault)
+EXTERN(__exc_memmanage)
+EXTERN(__exc_busfault)
+EXTERN(__exc_usagefault)
+EXTERN(__stm32reservedexception7)
+EXTERN(__stm32reservedexception8)
+EXTERN(__stm32reservedexception9)
+EXTERN(__stm32reservedexception10)
+EXTERN(__exc_svc)
+EXTERN(__exc_debug_monitor)
+EXTERN(__stm32reservedexception13)
+EXTERN(__exc_pendsv)
+EXTERN(__exc_systick)
 
-EXTERN (__cs3_stack)
-EXTERN (__cs3_reset)
-EXTERN (NMIException)
-EXTERN (HardFaultException)
-EXTERN (MemManageException)
-EXTERN (BusFaultException)
-EXTERN (UsageFaultException)
-EXTERN (__STM32ReservedException7)
-EXTERN (__STM32ReservedException8)
-EXTERN (__STM32ReservedException9)
-EXTERN (__STM32ReservedException10)
-EXTERN (SVCHandler)
-EXTERN (DebugMonitor)
-EXTERN (__STM32ReservedException13)
-EXTERN (PendSVC)
-EXTERN (SysTickHandler)
-EXTERN (WWDG_IRQHandler)
-EXTERN (PVD_IRQHandler)
-EXTERN (TAMPER_IRQHandler)
-EXTERN (RTC_IRQHandler)
-EXTERN (FLASH_IRQHandler)
-EXTERN (RCC_IRQHandler)
-EXTERN (EXTI0_IRQHandler)
-EXTERN (EXTI1_IRQHandler)
-EXTERN (EXTI2_IRQHandler)
-EXTERN (EXTI3_IRQHandler)
-EXTERN (EXTI4_IRQHandler)
-EXTERN (DMAChannel1_IRQHandler)
-EXTERN (DMAChannel2_IRQHandler)
-EXTERN (DMAChannel3_IRQHandler)
-EXTERN (DMAChannel4_IRQHandler)
-EXTERN (DMAChannel5_IRQHandler)
-EXTERN (DMAChannel6_IRQHandler)
-EXTERN (DMAChannel7_IRQHandler)
-EXTERN (ADC_IRQHandler)
-EXTERN (USB_HP_CAN_TX_IRQHandler)
-EXTERN (USB_LP_CAN_RX0_IRQHandler)
-EXTERN (CAN_RX1_IRQHandler)
-EXTERN (CAN_SCE_IRQHandler)
-EXTERN (EXTI9_5_IRQHandler)
-EXTERN (TIM1_BRK_IRQHandler)
-EXTERN (TIM1_UP_IRQHandler)
-EXTERN (TIM1_TRG_COM_IRQHandler)
-EXTERN (TIM1_CC_IRQHandler)
-EXTERN (TIM2_IRQHandler)
-EXTERN (TIM3_IRQHandler)
-EXTERN (TIM4_IRQHandler)
-EXTERN (I2C1_EV_IRQHandler)
-EXTERN (I2C1_ER_IRQHandler)
-EXTERN (I2C2_EV_IRQHandler)
-EXTERN (I2C2_ER_IRQHandler)
-EXTERN (SPI1_IRQHandler)
-EXTERN (SPI2_IRQHandler)
-EXTERN (USART1_IRQHandler)
-EXTERN (USART2_IRQHandler)
-EXTERN (USART3_IRQHandler)
-EXTERN (EXTI15_10_IRQHandler)
-EXTERN (RTCAlarm_IRQHandler)
-EXTERN (USBWakeUp_IRQHandler)
+EXTERN(__irq_wwdg)
+EXTERN(__irq_pvd)
+EXTERN(__irq_tamper)
+EXTERN(__irq_rtc)
+EXTERN(__irq_flash)
+EXTERN(__irq_rcc)
+EXTERN(__irq_exti0)
+EXTERN(__irq_exti1)
+EXTERN(__irq_exti2)
+EXTERN(__irq_exti3)
+EXTERN(__irq_exti4)
+EXTERN(__irq_dma_channel1)
+EXTERN(__irq_dma_channel2)
+EXTERN(__irq_dma_channel3)
+EXTERN(__irq_dma_channel4)
+EXTERN(__irq_dma_channel5)
+EXTERN(__irq_dma_channel6)
+EXTERN(__irq_dma_channel7)
+EXTERN(__irq_adc)
+EXTERN(__irq_usb_hp_can_tx)
+EXTERN(__irq_usb_lp_can_rx0)
+EXTERN(__irq_can_rx1)
+EXTERN(__irq_can_sce)
+EXTERN(__irq_exti9_5)
+EXTERN(__irq_tim1_brk)
+EXTERN(__irq_tim1_up)
+EXTERN(__irq_tim1_trg_com)
+EXTERN(__irq_tim1_cc)
+EXTERN(__irq_tim2)
+EXTERN(__irq_tim3)
+EXTERN(__irq_tim4)
+EXTERN(__irq_i2c1_ev)
+EXTERN(__irq_i2c1_er)
+EXTERN(__irq_i2c2_ev)
+EXTERN(__irq_i2c2_er)
+EXTERN(__irq_spi1)
+EXTERN(__irq_spi2)
+EXTERN(__irq_usart1)
+EXTERN(__irq_usart2)
+EXTERN(__irq_usart3)
+EXTERN(__irq_exti15_10)
+EXTERN(__irq_rtcalarm)
+EXTERN(__irq_usbwakeup)
+
+EXTERN(__irq_tim8_brk)
+EXTERN(__irq_tim8_up)
+EXTERN(__irq_tim8_trg_com)
+EXTERN(__irq_tim8_cc)
+EXTERN(__irq_adc3)
+EXTERN(__irq_fsmc)
+EXTERN(__irq_sdio)
+EXTERN(__irq_tim5)
+EXTERN(__irq_spi3)
+EXTERN(__irq_uart4)
+EXTERN(__irq_uart5)
+EXTERN(__irq_tim6)
+EXTERN(__irq_tim7)
+EXTERN(__irq_dma2_channel1)
+EXTERN(__irq_dma2_channel2)
+EXTERN(__irq_dma2_channel3)
+EXTERN(__irq_dma2_channel4_5)

libmaple/exc.S
@@ -41,16 +41,42 @@
 #    SP-->  r0
 
 .text
-.globl HardFaultException
+.globl __exc_hardfault
+.globl __exc_nmi
+.globl __exc_hardfault
+.globl __exc_memmanage
+.globl __exc_busfault
+.globl __exc_usagefault
+
+/* TODO: Fix this up. */
 .thumb_func
-HardFaultException:
-   ldr r0, CPSR_MASK           @ Set default CPSR
-   push {r0}
-   ldr r0, TARGET_PC           @ Set target pc
-   push {r0}
-   sub sp, sp, #24             @ its not like i even care
-   ldr r0, EXC_RETURN          @ Return to thread mode
-   mov lr, r0
+__exc_nmi:
+    mov r0, #1
+
+.thumb_func
+__exc_hardfault:
+    mov r0, #2
+
+.thumb_func
+__exc_memmanage:
+    mov r0, #3
+
+.thumb_func
+__exc_busfault:
+    mov r0, #4
+
+.thumb_func
+__exc_usagefault:
+    mov r0, #5
+
+__default:
+   ldr r1, CPSR_MASK           @ Set default CPSR
+   push {r1}
+   ldr r1, TARGET_PC           @ Set target pc
+   push {r1}
+   sub sp, sp, #24             @ Don't care
+   ldr r1, EXC_RETURN          @ Return to thread mode
+   mov lr, r1
    bx lr                       @ Exception exit
 
  .align 4

libmaple/exti.c
@@ -74,32 +74,32 @@ static inline void dispatch_handler(uint32 channel) {
  * is associated with each channel, so we
  * don't have to keep track of which channel
  * we came from */
-void EXTI0_IRQHandler(void) {
+void __irq_exti0(void) {
     dispatch_handler(EXTI0);
     clear_pending(EXTI0);
 }
 
-void EXTI1_IRQHandler(void) {
+void __irq_exti1(void) {
     dispatch_handler(EXTI1);
     clear_pending(EXTI1);
 }
 
-void EXTI2_IRQHandler(void) {
+void __irq_exti2(void) {
     dispatch_handler(EXTI2);
     clear_pending(EXTI2);
 }
 
-void EXTI3_IRQHandler(void) {
+void __irq_exti3(void) {
     dispatch_handler(EXTI3);
     clear_pending(EXTI3);
 }
 
-void EXTI4_IRQHandler(void) {
+void __irq_exti4(void) {
     dispatch_handler(EXTI4);
     clear_pending(EXTI4);
 }
 
-void EXTI9_5_IRQHandler(void) {
+void __irq_exti9_5(void) {
     /* Figure out which channel it came from  */
     uint32 pending;
     uint32 i;
@@ -116,7 +116,7 @@ void EXTI9_5_IRQHandler(void) {
     }
 }
 
-void EXTI15_10_IRQHandler(void) {
+void __irq_exti15_10(void) {
     /* Figure out which channel it came from  */
     uint32 pending;
     uint32 i;

libmaple/systick.c
@@ -64,6 +64,6 @@ void systick_resume() {
 }
 
 /** SysTick interrupt handler.  Bumps up the tick counter. */
-void SysTickHandler(void) {
+void __exc_systick(void) {
     systick_timer_millis++;
 }

libmaple/timers.c
@@ -382,7 +382,7 @@ void timer_generate_update(timer_dev_num timer_num) {
  * registers /or/ has overflowed.
  *
  * This is a rather long implementation... */
-void TIM1_CC_IRQHandler(void) {
+void __irq_tim1_cc(void) {
     timer_port *timer = (timer_port*)TIMER1_BASE;
     uint16 sr_buffer;
     sr_buffer = timer->SR;
@@ -418,7 +418,7 @@ void TIM1_CC_IRQHandler(void) {
         //timer->EGR  = 1;
     }
 }
-void TIM2_IRQHandler(void) {
+void __irq_tim2(void) {
     /* This is a rather long implementation... */
     timer_port *timer = (timer_port*)TIMER2_BASE;
     uint16 sr_buffer;
@@ -453,7 +453,7 @@ void TIM2_IRQHandler(void) {
         //timer->EGR  = 1;
     }
 }
-void TIM3_IRQHandler(void) {
+void __irq_tim3(void) {
     /* This is a rather long implementation... */
     timer_port *timer = (timer_port*)TIMER3_BASE;
     uint16 sr_buffer;
@@ -489,7 +489,7 @@ void TIM3_IRQHandler(void) {
     }
 }
 
-void TIM4_IRQHandler(void) {
+void __irq_tim4(void) {
     /* This is a rather long implementation... */
     timer_port*timer = (timer_port*)TIMER4_BASE;
     uint16 sr_buffer;

libmaple/usart.c
@@ -95,24 +95,24 @@ static inline void usart_irq(int usart_num) {
 /* TODO: Check the disassembly for the following functions to make
    sure GCC inlined properly. */
 
-void USART1_IRQHandler(void) {
+void __irq_usart1(void) {
     usart_irq(USART1);
 }
 
-void USART2_IRQHandler(void) {
+void __irq_usart2(void) {
     usart_irq(USART2);
 }
 
-void USART3_IRQHandler(void) {
+void __irq_usart3(void) {
     usart_irq(USART3);
 }
 
 #ifdef STM32_HIGH_DENSITY
-void UART4_IRQHandler(void) {
+void __irq_uart4(void) {
     usart_irq(UART4);
 }
 
-void UART5_IRQHandler(void) {
+void __irq_uart5(void) {
     usart_irq(UART5);
 }
 #endif

libmaple/usb/usb.c
@@ -241,7 +241,7 @@ void usbDsbISR(void) {
 }
 
 /* overloaded ISR routine, this is the main usb ISR */
-void usb_lpIRQHandler(void) {
+void __irq_usb_lp_can_rx0(void) {
 wIstr = _GetISTR();
 
 /* go nuts with the preproc switches since this is an ISTR and must be FAST */

rambutan32/librambutan

commit 7b95d3277074468dd1d22c21c4f558d04c2f7247

DMA: Fix non-working DMA interrupts.
libmaple/dma.c defines DMA interrupts __irq_dma_channel[1-7],
consistent with what is specified by support/ld/names.inc.  However,
names.inc is inconsistent with what support/ld/libcs3_stm32_src/
expects.  Specifically, it contradicts the files

- support/ld/libcs3_stm32_src/stm32_isrs.S
- support/ld/libcs3_stm32_src/stm32_vector_table.S

Which use the names __irq_dma1_channel[1-7].

Change names.inc and dma.c to use the correct IRQ names.

The original names.inc/libcs3_stm32_src inconsistency was introduced
in 43d6921658cd29b8022af4424d340a90fbcb9a7f, but dma.c had the correct
names until ec3cf2903f4b03bc1dae5e159495c9e5ef0938ca, where they were
renamed for consistency with names.inc.  At that point, DMA interrupts
stopped working.  (This was documented in the commit message).

Thanks to forum user robodude666 for tracking this down.

libmaple/dma.c
@@ -331,31 +331,31 @@ static inline void dispatch_handler(dma_dev *dev, dma_channel channel) {
     }
 }
 
-void __irq_dma_channel1(void) {
+void __irq_dma1_channel1(void) {
     dispatch_handler(DMA1, DMA_CH1);
 }
 
-void __irq_dma_channel2(void) {
+void __irq_dma1_channel2(void) {
     dispatch_handler(DMA1, DMA_CH2);
 }
 
-void __irq_dma_channel3(void) {
+void __irq_dma1_channel3(void) {
     dispatch_handler(DMA1, DMA_CH3);
 }
 
-void __irq_dma_channel4(void) {
+void __irq_dma1_channel4(void) {
     dispatch_handler(DMA1, DMA_CH4);
 }
 
-void __irq_dma_channel5(void) {
+void __irq_dma1_channel5(void) {
     dispatch_handler(DMA1, DMA_CH5);
 }
 
-void __irq_dma_channel6(void) {
+void __irq_dma1_channel6(void) {
     dispatch_handler(DMA1, DMA_CH6);
 }
 
-void __irq_dma_channel7(void) {
+void __irq_dma1_channel7(void) {
     dispatch_handler(DMA1, DMA_CH7);
 }
 

support/ld/names.inc
@@ -26,13 +26,13 @@ EXTERN(__irq_exti1)
 EXTERN(__irq_exti2)
 EXTERN(__irq_exti3)
 EXTERN(__irq_exti4)
-EXTERN(__irq_dma_channel1)
-EXTERN(__irq_dma_channel2)
-EXTERN(__irq_dma_channel3)
-EXTERN(__irq_dma_channel4)
-EXTERN(__irq_dma_channel5)
-EXTERN(__irq_dma_channel6)
-EXTERN(__irq_dma_channel7)
+EXTERN(__irq_dma1_channel1)
+EXTERN(__irq_dma1_channel2)
+EXTERN(__irq_dma1_channel3)
+EXTERN(__irq_dma1_channel4)
+EXTERN(__irq_dma1_channel5)
+EXTERN(__irq_dma1_channel6)
+EXTERN(__irq_dma1_channel7)
 EXTERN(__irq_adc)
 EXTERN(__irq_usb_hp_can_tx)
 EXTERN(__irq_usb_lp_can_rx0)