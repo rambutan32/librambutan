8038a6e3386d59af6a1a099e21234a93be15ef15
bryan newbold <bnewbold@robocracy.org>
2015-06-12 03:06:02 -0700

    wirish: enable HardwareSPI for STM32F2-4

--- wirish/include/wirish/wirish.h
+++ wirish/include/wirish/wirish.h
@@ -44,9 +44,7 @@
 #include <wirish/wirish_debug.h>
 #include <wirish/wirish_math.h>
 #include <wirish/wirish_time.h>
-#if STM32_MCU_SERIES == STM32_SERIES_F1 /* FIXME [0.0.13?] port to F2 */
 #include <wirish/HardwareSPI.h>
-#endif
 #include <wirish/HardwareSerial.h>
 #include <wirish/HardwareTimer.h>
 #include <wirish/usb_serial.h>
